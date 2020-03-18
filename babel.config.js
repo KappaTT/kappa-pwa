ce36dfc121c92c96246129a2b98c9c9aee93b4a4
Jeff Taylor-Chang <jeff@jefftc.com>
2020-03-18 11:11:47 -0400

    Added aliasing

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,36 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          alias: {
+            '@backend': './src/backend',
+            '@backend/*': './src/backend/*',
+            '@components': './src/components',
+            '@components/*': './src/components/*',
+            '@constants': './src/constants',
+            '@constants/*': './src/constants/*',
+            '@navigation': './src/navigation',
+            '@navigation/*': './src/navigation/*',
+            '@pages': './src/pages',
+            '@pages/*': './src/pages/*',
+            '@reducers': './src/reducers',
+            '@reducers/*': './src/reducers/*',
+            '@screens': './src/screens',
+            '@screens/*': './src/screens/*',
+            '@services': './src/services',
+            '@services/*': './src/services/*',
+            '@types': './src/types',
+            '@types/*': './src/types/*',
+            '@argon': './libs/argon',
+            '@argon/*': './libs/argon/*',
+            '@galio': './libs/galio',
+            '@galio/*': './libs/galio/*'
+          }
+        }
+      ]
+    ]
   };
 };
