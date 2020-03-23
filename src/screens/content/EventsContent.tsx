106195a945308d9ab391e3cd07979bd83e513a13
Jeff Taylor-Chang <jeff@jefftc.com>
2020-03-23 19:18:09 -0400

    Added Header to EventsContent

--- src/screens/content/EventsContent.tsx
+++ src/screens/content/EventsContent.tsx
@@ -4,9 +4,9 @@ import { Placeholder, PlaceholderLine, Fade } from 'rn-placeholder';
 import { useSafeArea } from 'react-native-safe-area-context';
 
 import { theme } from '@constants';
-import { Block, Text } from '@components';
+import { Block, Header, Text } from '@components';
 import { NavigationTypes } from '@types';
-import { TabBarHeight } from '@services/utils';
+import { TabBarHeight, HeaderHeight } from '@services/utils';
 
 const EventSkeleton: React.FC<{}> = ({}) => {
   return (
@@ -26,16 +26,22 @@ const EventsContent: React.FC<{
 }> = ({ navigation }) => {
   const insets = useSafeArea();
 
+  const loading = true;
+
   return (
     <Block flex>
+      <Header title="Upcoming Events" />
+
       <Block
-        style={{
-          top: insets.top,
-          bottom: TabBarHeight + insets.bottom
-        }}
+        style={[
+          styles.wrapper,
+          {
+            top: insets.top + HeaderHeight
+          }
+        ]}
       >
-        <ScrollView refreshControl={<RefreshControl refreshing={false} onRefresh={() => {}} />}>
-          <Block style={styles.container}>
+        {loading ? (
+          <Block style={styles.loadingContainer}>
             <EventSkeleton />
             <Block style={styles.eventSeparator} />
             <EventSkeleton />
@@ -46,13 +52,24 @@ const EventsContent: React.FC<{
             <Block style={styles.eventSeparator} />
             <EventSkeleton />
           </Block>
-        </ScrollView>
+        ) : (
+          <ScrollView refreshControl={<RefreshControl refreshing={false} onRefresh={() => {}} />}>
+            <Block style={styles.container}></Block>
+          </ScrollView>
+        )}
       </Block>
     </Block>
   );
 };
 
 const styles = StyleSheet.create({
+  wrapper: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    bottom: 0
+  },
+  loadingContainer: {},
   skeletonWrapper: {
     marginHorizontal: 8,
     marginVertical: 4,
