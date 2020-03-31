29115cff5275a8c8602c31976ad2e9bd87952e96
Jeff Taylor-Chang <jeff@jefftc.com>
2020-03-31 17:44:25 -0400

    Added support for events to reducers

--- src/reducers/actions/kappa.ts
+++ src/reducers/actions/kappa.ts
@@ -1,3 +1,39 @@
-export const loadEvents = () => {};
+import { Kappa } from '@backend';
+import { GET_EVENTS, GET_EVENTS_SUCCESS, GET_EVENTS_FAILURE } from '@reducers/kappa';
+import { TUser } from '@backend/auth';
 
-export const loadDirectory = () => {};
+const gettingEvents = () => {
+  return {
+    type: GET_EVENTS
+  };
+};
+
+const getEventsSuccess = data => {
+  return {
+    type: GET_EVENTS_SUCCESS,
+    events: data.events
+  };
+};
+
+const getEventsFailure = err => {
+  return {
+    type: GET_EVENTS_FAILURE,
+    error: err
+  };
+};
+
+export const getEvents = (user: TUser) => {
+  return dispatch => {
+    dispatch(gettingEvents());
+
+    Kappa.getEvents({ user }).then(res => {
+      if (res.success) {
+        dispatch(getEventsSuccess(res.data));
+      } else {
+        dispatch(getEventsFailure(res.error));
+      }
+    });
+  };
+};
+
+export const getDirectory = () => {};
--- src/reducers/kappa.ts
+++ src/reducers/kappa.ts
@@ -1,15 +1,49 @@
+import { TEvent } from '@backend/kappa';
+
+export const GET_EVENTS = 'GET_EVENTS';
+export const GET_EVENTS_SUCCESS = 'GET_EVENTS_SUCCESS';
+export const GET_EVENTS_FAILURE = 'GET_EVENTS_FAILURE';
+
 export interface TKappaState {
-  events: [];
+  gettingEvents: boolean;
+  getEventsError: boolean;
+  getEventsErrorMessage: string;
+
+  events: Array<TEvent>;
   directory: [];
 }
 
 const initialState: TKappaState = {
+  gettingEvents: false,
+  getEventsError: false,
+  getEventsErrorMessage: '',
+
   events: [],
   directory: []
 };
 
 export default (state = initialState, action: any): TKappaState => {
   switch (action.type) {
+    case GET_EVENTS:
+      return {
+        ...state,
+        gettingEvents: true,
+        getEventsError: false,
+        getEventsErrorMessage: ''
+      };
+    case GET_EVENTS_SUCCESS:
+      return {
+        ...state,
+        gettingEvents: false,
+        events: action.events
+      };
+    case GET_EVENTS_FAILURE:
+      return {
+        ...state,
+        gettingEvents: false,
+        getEventsError: true,
+        getEventsErrorMessage: action.error.message
+      };
     default:
       return state;
   }
