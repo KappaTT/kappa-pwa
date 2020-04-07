1c11104e9fa6421ebb6375b64e306e1c52fae48e
Jeff Taylor-Chang <jeff@jefftc.com>
2020-04-07 19:48:12 -0400

    Added fields to check in content

--- src/screens/content/CheckInContent.tsx
+++ src/screens/content/CheckInContent.tsx
@@ -8,7 +8,18 @@ import { theme } from '@constants';
 import { TRedux } from '@reducers';
 import { _kappa } from '@reducers/actions';
 import { NavigationTypes } from '@types';
-import { Block, Text, Header, Icon, ListButton, RoundButton, SlideModal, RadioList } from '@components';
+import {
+  Block,
+  Text,
+  Header,
+  Icon,
+  ListButton,
+  RoundButton,
+  SlideModal,
+  RadioList,
+  FormattedInput,
+  KeyboardDismissView
+} from '@components';
 import { HeaderHeight } from '@services/utils';
 import { getEventById, hasValidCheckIn, shouldLoad, sortEventByDate } from '@services/kappaService';
 
@@ -24,6 +35,8 @@ const CheckInContent: React.FC<{
   const checkInExcuse = useSelector((state: TRedux) => state.kappa.checkInExcuse);
 
   const [choosingEvent, setChoosingEvent] = React.useState<boolean>(false);
+  const [code, setCode] = React.useState<string>('');
+  const [excuse, setExcuse] = React.useState<string>('');
 
   const dispatch = useDispatch();
   const dispatchSetCheckInEvent = React.useCallback(
@@ -70,6 +83,10 @@ const CheckInContent: React.FC<{
     },
     [checkInExcuse]
   );
+
+  const numberFormatter = (text: string) => {
+    return text !== undefined ? text.replace(/\D/g, '') : '';
+  };
 
   React.useEffect(() => {
     if (shouldLoad(loadHistory, user.email) && !gettingAttendance) {
@@ -120,68 +137,111 @@ const CheckInContent: React.FC<{
   };
 
   return (
-    <Block flex>
-      <Header title="Check In" />
+    <KeyboardDismissView style={styles.flex}>
+      <Block flex>
+        <Header title="Check In" />
 
-      <Block
-        style={[
-          styles.wrapper,
-          {
-            top: insets.top + HeaderHeight
-          }
-        ]}
-      >
-        <Block style={styles.content}>
-          <Block style={styles.eventIdContainer}>
-            <ListButton
-              keyText="Event"
-              valueText={selectedEvent === null ? 'choose one' : selectedEvent.title}
-              onPress={() => setChoosingEvent(true)}
-            />
+        <Block
+          style={[
+            styles.wrapper,
+            {
+              top: insets.top + HeaderHeight
+            }
+          ]}
+        >
+          <Block style={styles.content}>
+            <Block style={styles.eventIdContainer}>
+              <ListButton
+                keyText="Event"
+                valueText={selectedEvent === null ? 'choose one' : selectedEvent.title}
+                onPress={() => setChoosingEvent(true)}
+              />
+            </Block>
+
+            <Block style={styles.checkInContainer}>
+              <Block style={styles.propertyHeaderContainer}>
+                <Text style={styles.propertyHeader}>Check in</Text>
+              </Block>
+
+              <FormattedInput
+                style={styles.input}
+                placeholderText="code"
+                maxLength={4}
+                keyboardType="number-pad"
+                error={false}
+                defaultValue={code}
+                formatter={numberFormatter}
+                onChangeText={(text: string) => setCode(text)}
+              />
+            </Block>
+
+            <Block style={styles.dividerWrapper}>
+              <Block style={styles.divider} />
+              <Text style={styles.orDividerText}>OR</Text>
+              <Block style={styles.divider} />
+            </Block>
+
+            <Block style={styles.excuseContainer}>
+              <Block style={styles.propertyHeaderContainer}>
+                <Text style={styles.propertyHeader}>Excuse</Text>
+              </Block>
+
+              <FormattedInput
+                style={styles.input}
+                placeholderText="reason"
+                maxLength={128}
+                error={false}
+                defaultValue={excuse}
+                onChangeText={(text: string) => setExcuse(text)}
+              />
+            </Block>
+          </Block>
+
+          <Block style={styles.bottomBar}>
+            {alreadyCheckedIn ? (
+              <React.Fragment>
+                <Icon family="Feather" name="check" size={24} color={theme.COLORS.PRIMARY_GREEN} />
+                <Text style={styles.alreadyCheckedIn}>Checked In</Text>
+              </React.Fragment>
+            ) : (
+              <React.Fragment>
+                <Block style={styles.excuseButton}>
+                  <RoundButton
+                    alt={true}
+                    label="Request Excuse"
+                    disabled={selectedEvent === null || needsLoading || !selectedEvent.excusable || alreadyCheckedIn}
+                    onPress={() => {}}
+                  />
+                </Block>
+                <Block style={styles.bottomDivider} />
+
+                <Block style={styles.attendButton}>
+                  <RoundButton
+                    disabled={
+                      selectedEvent === null ||
+                      needsLoading ||
+                      alreadyCheckedIn ||
+                      moment(selectedEvent.start).isSame(moment(), 'day')
+                    }
+                    label="Check In"
+                    onPress={() => {}}
+                  />
+                </Block>
+              </React.Fragment>
+            )}
           </Block>
         </Block>
 
-        <Block style={styles.bottomBar}>
-          {alreadyCheckedIn ? (
-            <React.Fragment>
-              <Icon family="Feather" name="check" size={24} color={theme.COLORS.PRIMARY_GREEN} />
-              <Text style={styles.alreadyCheckedIn}>Checked In</Text>
-            </React.Fragment>
-          ) : (
-            <React.Fragment>
-              <Block style={styles.excuseButton}>
-                <RoundButton
-                  alt={true}
-                  label="Request Excuse"
-                  disabled={selectedEvent === null || needsLoading || !selectedEvent.excusable || alreadyCheckedIn}
-                  onPress={() => {}}
-                />
-              </Block>
-              <Block style={styles.bottomDivider} />
-
-              <Block style={styles.attendButton}>
-                <RoundButton
-                  disabled={
-                    selectedEvent === null ||
-                    needsLoading ||
-                    alreadyCheckedIn ||
-                    moment(selectedEvent.start).isSame(moment(), 'day')
-                  }
-                  label="Check In"
-                  onPress={() => {}}
-                />
-              </Block>
-            </React.Fragment>
-          )}
-        </Block>
+        <SlideModal visible={choosingEvent}>{renderChoosingEvent()}</SlideModal>
       </Block>
-
-      <SlideModal visible={choosingEvent}>{renderChoosingEvent()}</SlideModal>
-    </Block>
+    </KeyboardDismissView>
   );
 };
 
 const styles = StyleSheet.create({
+  flex: {
+    flex: 1
+  },
   wrapper: {
     position: 'absolute',
     left: 0,
@@ -238,7 +298,36 @@ const styles = StyleSheet.create({
     fontFamily: 'OpenSans-SemiBold',
     fontSize: 18,
     color: theme.COLORS.PRIMARY_GREEN
-  }
+  },
+  checkInContainer: {
+    marginTop: 48
+  },
+  input: {
+    backgroundColor: theme.COLORS.SUPER_LIGHT_BLUE_GRAY
+  },
+  multiInput: {
+    backgroundColor: theme.COLORS.SUPER_LIGHT_BLUE_GRAY,
+    height: 128
+  },
+  dividerWrapper: {
+    marginHorizontal: 8,
+    marginVertical: 20,
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  divider: {
+    flexGrow: 1,
+    borderBottomColor: theme.COLORS.BORDER,
+    borderBottomWidth: 1
+  },
+  orDividerText: {
+    marginHorizontal: 8,
+    fontFamily: 'OpenSans',
+    color: theme.COLORS.BORDER
+  },
+  excuseContainer: {}
 });
 
 export default CheckInContent;
