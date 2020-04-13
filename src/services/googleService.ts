762ab806953b51fd7f59c97c3ece3dfbdf748ea7
Jeff Taylor-Chang <jeff@jefftc.com>
2020-04-13 09:15:07 -0400

    Added standalone ios client id for google service

--- src/services/googleService.ts
+++ src/services/googleService.ts
@@ -6,11 +6,14 @@ export const login = async () => {
   try {
     const loginResult = await Google.logInAsync({
       iosClientId: '223233671218-ceilcecpn0t04ec5or3tk680pfoomf4v.apps.googleusercontent.com',
-      androidClientId: '223233671218-joevmt53u95c0o70mttjrodcbd5nj23j.apps.googleusercontent.com'
+      androidClientId: '223233671218-joevmt53u95c0o70mttjrodcbd5nj23j.apps.googleusercontent.com',
+      iosStandaloneAppClientId: '',
+      androidStandaloneAppClientId: ''
     });
 
     if (loginResult.type === 'success') {
       log(loginResult);
+
       return {
         success: true,
         data: {
