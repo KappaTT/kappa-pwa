50328c7be2da4501f2f3bf301769928c21e630d7
Jeff Taylor-Chang <jeff@jefftc.com>
2020-04-15 08:46:07 -0400

    Added sentry config

--- App.tsx
+++ App.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import * as Sentry from 'sentry-expo';
+import Constants from 'expo-constants';
 import { Provider } from 'react-redux';
 import { AppearanceProvider } from 'react-native-appearance';
 
 import App from './src/App';
 import store from '@reducers';
+
+Sentry.init({
+  dsn: 'https://83abd164c9094221ba93a377464f3fe4@o378213.ingest.sentry.io/5201293',
+  enableInExpoDevelopment: false,
+  debug: true
+});
+
+Sentry.setRelease(Constants.manifest.revisionId);
 
 const AppWrapper = () => {
   return (
