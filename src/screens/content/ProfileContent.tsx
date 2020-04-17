c63fcee435c73bf5410e4da8d6dc7fa1dbf0359f
Jeff Taylor-Chang <jeff@jefftc.com>
2020-04-17 13:58:09 -0400

    Started building profile page

--- src/screens/content/ProfileContent.tsx
+++ src/screens/content/ProfileContent.tsx
@@ -1,24 +1,74 @@
 import React from 'react';
-import { StyleSheet, ScrollView } from 'react-native';
+import { StyleSheet, ScrollView, TouchableOpacity } from 'react-native';
+import { useSelector, useDispatch } from 'react-redux';
+import { useSafeArea } from 'react-native-safe-area-context';
 
+import { TRedux } from '@reducers';
+import { _auth } from '@reducers/actions';
 import { theme } from '@constants';
-import { Block, Text } from '@components';
+import { Block, Text, Icon } from '@components';
 import { NavigationTypes } from '@types';
+import { log } from '@services/logService';
 
 const ProfileContent: React.FC<{
   navigation: NavigationTypes.ParamType;
 }> = ({ navigation }) => {
+  const user = useSelector((state: TRedux) => state.auth.user);
+
+  const dispatch = useDispatch();
+  const dispatchSignOut = React.useCallback(() => dispatch(_auth.signOut()), [dispatch]);
+
+  const insets = useSafeArea();
+
   return (
     <Block flex>
       <ScrollView>
-        <Block flex middle>
-          <Text>Content goes here...</Text>
+        <Block
+          style={[
+            styles.content,
+            {
+              paddingTop: insets.top + 20
+            }
+          ]}
+        >
+          <Block style={styles.header}>
+            <Block style={styles.headerTextWrapper}>
+              <Text style={styles.title}>Hi {user.givenName}</Text>
+              <Text style={styles.subtitle}>{user.email}</Text>
+            </Block>
+
+            <TouchableOpacity onPress={() => log('todo')}>
+              <Icon style={styles.editButton} family="MaterialIcons" name="edit" size={24} />
+            </TouchableOpacity>
+          </Block>
         </Block>
       </ScrollView>
     </Block>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  content: {
+    paddingHorizontal: 20
+  },
+  header: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  headerTextWrapper: {},
+  title: {
+    fontFamily: 'OpenSans-Bold',
+    fontSize: 24
+  },
+  subtitle: {
+    fontFamily: 'OpenSans',
+    fontSize: 13,
+    color: theme.COLORS.GRAY
+  },
+  editButton: {
+    paddingTop: 8
+  }
+});
 
 export default ProfileContent;
