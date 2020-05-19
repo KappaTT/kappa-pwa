61d432aba2080bea8d0f86fb18f18c72fc93d80e
Jeff Taylor-Chang <jeff@jefftc.com>
2020-05-19 13:53:44 -0400

    Updated secure storage import

--- src/services/secureStorage.ts
+++ src/services/secureStorage.ts
@@ -1,4 +1,4 @@
-import * as SecureStore from 'expo-secure-store';
+import { setItemAsync, getItemAsync, deleteItemAsync } from 'expo-secure-store';
 
 import {
   setItemTemplate,
@@ -9,15 +9,15 @@ import {
   deleteBatchTemplate
 } from '@services/storage';
 
-export const setItem = (key: string, value: any) => setItemTemplate(SecureStore.setItemAsync, key, value);
+export const setItem = (key: string, value: any) => setItemTemplate(setItemAsync, key, value);
 
 export const setBatch = (parent: string, data: any) => setBatchTemplate(setItem, parent, data);
 
-export const getItem = (key: string) => getItemTemplate(SecureStore.getItemAsync, key);
+export const getItem = (key: string) => getItemTemplate(getItemAsync, key);
 
 export const getBatch = (parent: string, defaultData: any, force: boolean = false) =>
   getBatchTemplate(getItem, parent, defaultData, force);
 
-export const deleteItem = (key: string) => deleteItemTemplate(SecureStore.deleteItemAsync, key);
+export const deleteItem = (key: string) => deleteItemTemplate(deleteItemAsync, key);
 
 export const deleteBatch = (parent: string, data: any) => deleteBatchTemplate(deleteItem, parent, data);
