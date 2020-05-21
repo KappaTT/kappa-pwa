8085f1af2b4055579b2f0681fc1a2c812fa0e0ab
Jeff Taylor-Chang <jeff@jefftc.com>
2020-05-21 14:40:18 -0400

    Moved navigation options into stack

--- src/navigation/MainTabNavigator.tsx
+++ src/navigation/MainTabNavigator.tsx
@@ -7,85 +7,100 @@ import { theme } from '@constants';
 import { Icon, TabBar } from '@components';
 import { MessagesScreen, CheckInScreen, DirectoryScreen, EventsScreen, ProfileScreen } from '@screens';
 
-const EventsStack = createStackNavigator({
-  Events: {
-    screen: EventsScreen,
+const EventsStack = createStackNavigator(
+  {
+    Events: {
+      screen: EventsScreen,
+      navigationOptions: ({ navigation }) => ({
+        header: null
+      })
+    }
+  },
+  {
     navigationOptions: ({ navigation }) => ({
-      header: null
+      tabBarLabel: 'Events',
+      tabBarIcon: ({ tintColor }) => (
+        <Icon style={styles.iconEvents} name="calendar" family="Feather" color={tintColor} size={28} />
+      )
     })
   }
-});
+);
 
-EventsStack.navigationOptions = ({ navigation }) => ({
-  tabBarLabel: 'Events',
-  tabBarIcon: ({ tintColor }) => (
-    <Icon style={styles.iconEvents} name="calendar" family="Feather" color={tintColor} size={28} />
-  )
-});
-
-const DirectoryStack = createStackNavigator({
-  Directory: {
-    screen: DirectoryScreen,
+const DirectoryStack = createStackNavigator(
+  {
+    Directory: {
+      screen: DirectoryScreen,
+      navigationOptions: ({ navigation }) => ({
+        header: null
+      })
+    }
+  },
+  {
     navigationOptions: ({ navigation }) => ({
-      header: null
+      tabBarLabel: 'Directory',
+      tabBarIcon: ({ tintColor }) => (
+        <Icon style={styles.iconDirectory} name="contacts" family="AntDesign" color={tintColor} size={28} />
+      )
     })
   }
-});
+);
 
-DirectoryStack.navigationOptions = ({ navigation }) => ({
-  tabBarLabel: 'Directory',
-  tabBarIcon: ({ tintColor }) => (
-    <Icon style={styles.iconDirectory} name="contacts" family="AntDesign" color={tintColor} size={28} />
-  )
-});
-
-const CheckInStack = createStackNavigator({
-  CheckIn: {
-    screen: CheckInScreen,
+const CheckInStack = createStackNavigator(
+  {
+    CheckIn: {
+      screen: CheckInScreen,
+      navigationOptions: ({ navigation }) => ({
+        header: null
+      })
+    }
+  },
+  {
     navigationOptions: ({ navigation }) => ({
-      header: null
+      tabBarLabel: 'Check In',
+      tabBarIcon: ({ tintColor }) => (
+        <Icon style={styles.iconCheckIn} name="check-square" family="Feather" color={tintColor} size={28} />
+      )
     })
   }
-});
+);
 
-CheckInStack.navigationOptions = ({ navigation }) => ({
-  tabBarLabel: 'Check In',
-  tabBarIcon: ({ tintColor }) => (
-    <Icon style={styles.iconCheckIn} name="check-square" family="Feather" color={tintColor} size={28} />
-  )
-});
-
-const MessagesStack = createStackNavigator({
-  Messages: {
-    screen: MessagesScreen,
+const MessagesStack = createStackNavigator(
+  {
+    Messages: {
+      screen: MessagesScreen,
+      navigationOptions: ({ navigation }) => ({
+        header: null
+      })
+    }
+  },
+  {
     navigationOptions: ({ navigation }) => ({
-      header: null
+      tabBarLabel: 'Messages',
+      tabBarIcon: ({ tintColor }) => (
+        <Icon style={styles.iconMessages} name="message-square" family="Feather" color={tintColor} size={28} />
+      )
     })
   }
-});
+);
 
-MessagesStack.navigationOptions = ({ navigation }) => ({
-  tabBarLabel: 'Messages',
-  tabBarIcon: ({ tintColor }) => (
-    <Icon style={styles.iconMessages} name="message-square" family="Feather" color={tintColor} size={28} />
-  )
-});
-
-const ProfileStack = createStackNavigator({
-  Profile: {
-    screen: ProfileScreen,
+const ProfileStack = createStackNavigator(
+  {
+    Profile: {
+      screen: ProfileScreen,
+      navigationOptions: ({ navigation }) => ({
+        header: null
+      })
+    }
+  },
+  {
     navigationOptions: ({ navigation }) => ({
-      header: null
+      tabBarLabel: 'Profile',
+      tabBarIcon: ({ tintColor }) => (
+        <Icon style={styles.iconProfile} name="user" family="Feather" color={tintColor} size={28} />
+      )
     })
   }
-});
-
-ProfileStack.navigationOptions = ({ navigation }) => ({
-  tabBarLabel: 'Profile',
-  tabBarIcon: ({ tintColor }) => (
-    <Icon style={styles.iconProfile} name="user" family="Feather" color={tintColor} size={28} />
-  )
-});
+);
 
 const styles = StyleSheet.create({
   iconEvents: {},
