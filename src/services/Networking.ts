14f95680027311d006f856e360f298b39d393dd0
Jeff Taylor-Chang <jeff@jefftc.com>
2020-12-26 17:55:47 -0500

    Added cors mode to fetch

--- src/services/Networking.ts
+++ src/services/Networking.ts
@@ -87,6 +87,7 @@ export const jsonRequest = async <T>(
   try {
     const response = await fetch(url, {
       method,
+      mode: 'cors',
       cache: 'no-store',
       headers: {
         ...headers,
