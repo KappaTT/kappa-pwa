55d2751542b4e0042b8f1772643908ad7cc2c1ff
Jeff Taylor-Chang <jeff@jefftc.com>
2020-12-28 10:11:01 -0500

    Fixed switch coloring

--- src/components/Switch.tsx
+++ src/components/Switch.tsx
@@ -1,32 +1,43 @@
 import React from 'react';
-import { Switch as NativeSwitch, Platform } from 'react-native';
+import { Switch as NativeSwitch, TouchableOpacity } from 'react-native';
 
 import { theme } from '@constants';
 
 const Switch: React.FC<{
   value?: boolean;
+  disabled?: boolean;
   thumbColor?: string;
   backgroundColor?: string;
   trackColor?: string;
   onValueChange?(newValue: boolean): void;
 }> = ({
   value = false,
+  disabled = false,
   thumbColor = theme.COLORS.PRIMARY,
-  backgroundColor = theme.COLORS.MAIN_GRAY,
+  backgroundColor = theme.COLORS.LIGHT_BORDER,
   trackColor = theme.COLORS.PRIMARY,
   onValueChange = (newValue: boolean) => {}
 }) => {
-  const _thumbColor =
-    Platform.OS === 'ios' ? null : Platform.OS === 'android' && value ? thumbColor : theme.COLORS.DARK_GRAY;
+  const onPress = React.useCallback(() => {
+    // Using touchable to intercept and mimic behavior due to bug that causes focus overlay on switch if pressed
+    if (!disabled) onValueChange(!value);
+  }, [disabled, onValueChange, value]);
 
   return (
-    <NativeSwitch
-      value={value}
-      thumbColor={_thumbColor}
-      ios_backgroundColor={backgroundColor}
-      trackColor={{ false: trackColor, true: trackColor }}
-      onValueChange={onValueChange}
-    />
+    <TouchableOpacity style={disabled && { opacity: 0.6 }} activeOpacity={0.8} onPress={onPress}>
+      <NativeSwitch
+        pointerEvents="none"
+        value={value}
+        disabled={disabled}
+        thumbTintColor={thumbColor}
+        thumbColor={thumbColor}
+        // @ts-ignore
+        activeThumbColor={thumbColor}
+        onTintColor={`${trackColor}50`}
+        tintColor={backgroundColor}
+        trackColor={{ false: backgroundColor, true: trackColor }}
+      />
+    </TouchableOpacity>
   );
 };
 
